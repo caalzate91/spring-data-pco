bee3d8bd90020d25378fd89c62738ee760d28c5c
Camilo Alzate <caalzateg@gmail.com>
2025-01-02 14:42:57 -0500

    [add] Add H2 conection

---
+++ src/test/java/com/banking/accounting/AccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
